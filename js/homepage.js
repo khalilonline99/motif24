0e1aec57aab09dda6300a4b72906a0abeb488a62
khalilonline99 <khalilonline99@gmail.com>
2024-10-19 01:27:54 +0600

    quick

--- js/homepage.js
+++ js/homepage.js
@@ -210,7 +210,7 @@ window.onload = function () {
                     pagination: false,
                     navigation: false,
                     spaceBetween: 0,
-                    slidesOffsetBefore: 100,
+                    slidesOffsetBefore: itemLeftMargin,
                     slidesPerView: 4,
                     breakpoints: {
                         320: {
