304006b5bf136c92bc05aad066830d4ae1e2c55b
khalilonline99 <khalilonline99@gmail.com>
2024-11-12 19:14:20 +0600

    quick

--- functions.php
+++ functions.php
@@ -33,7 +33,7 @@ function theme_gsap_script()
     wp_enqueue_script('gsap-ce', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/CustomEase.min.js', array('gsap-js'), false, true);
     wp_enqueue_script('gsap-stp', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollToPlugin.min.js', array('gsap-js'), false, true);
     // Your animation code file - with gsap.js passed as a dependency
-    wp_enqueue_script('gsap-js2', get_template_directory_uri() . '/js/homepage.js', array('gsap-js'), false, true);
+    // wp_enqueue_script('gsap-js2', get_template_directory_uri() . '/js/homepage.js', array('gsap-js'), false, true);
 }
 
 add_action('wp_enqueue_scripts', 'theme_gsap_script');
